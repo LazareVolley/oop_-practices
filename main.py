1db0674bd0d2e12359db8d2da3f8157ef9812b41
LazareVolley <LazareVolley@yandex.ru>
2024-05-06 01:39:55 +0300

    trird example

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import random
+
 #Задача №1
 class Animal:
     def __init__(self, name: str, type: str, age: int, sound: str):
+        """
+        Конструктор
+        :return: None
+        """
         self.name = name
         self.type = type
         self.age = age
@@ -14,6 +20,10 @@ class Animal:
         print(f"{self.name} издаёт звук: {self.sound}")
 
     def animal_information(self):
+        """
+        Вывод полей констурктора (Ифнормация о животном)
+        :return: None
+        """
         print(f"Кличка: {self.name}")
         print(f"Вид животного:{self.type}")
         print(f"Возраст: {self.age}")
@@ -28,14 +38,18 @@ print(animals.animal_information())
 
 class Book:
     def __init__(self, title: str, author: str, page_count: int):
+        """
+        Конструктор
+        :return: None
+        """
         self.title = title
         self.author = author
         self.page_count = page_count
 
     def book_info(self):
         """
-        Вывод полей метода конструктора
-        :return:
+        Вывод полей конструктора (Информации о книге)
+        :return: None
         """
         print(f"Название книги: '{self.title}'")
         print(f"Автор книги: {self.author}")
@@ -63,3 +77,74 @@ class Book:
 books = Book("Питон для начинающих", "Артур Пирожков", 80)
 print(books.book_info())
 print(books.open_page())
+
+#Задача №3
+
+class PassengerPlane:
+    def __init__(self, manufacturer: str, plane_model: str, capacity: int, current_height: float, current_speed: int):
+        """
+        Конструктор
+        :return: None
+        """
+        self.manufacturer = manufacturer
+        self.plane_model = plane_model
+        self.capacity = capacity
+        self.current_height = current_height
+        self.current_speed = current_speed
+
+    def plane_information(self):
+        """
+        Вывод полей конструктора (Информации о самолёте)
+        :return: None
+        """
+        print(f"Производитель: {self.manufacturer}")
+        print(f"Модель самолёта: {self.plane_model} тип: пассажирский")
+        print(f"Вместимость самолёта (Количество сидячих мест): {self.capacity} чел.")
+        print(f"Текущая высота самолёта: {self.current_height}")
+        print(f"Текущая скорость самолёта: {self.current_speed}")
+
+
+    def airplane_takeoff(self):
+        """
+        Вывод полей конструктора (имитация взлёта самолё та)
+        :return: None
+        """
+        self.current_height = f"{0.0} --> {random.uniform(2, 3)} --> {random.uniform(6, 8)}"
+        self.current_speed = f"{0.0} --> {random.uniform(340, 360)}"
+        print(f"Уважаемые пассажиры! Самолёт '{self.plane_model}' осуществляет взлёт! Текущая скорость: {self.current_speed}км/ч. Текущая высота: {self.current_height}м.")
+
+    def airplane_flight(self):
+        """
+        Вывод полей конструктора (имитация полёта самолёта)
+        :return:
+        """
+        self.current_height = random.uniform(860, 920)
+        self.current_speed = f"{0.0} --> {random.uniform(840, 920)}"
+        print(f"Уважаемые пассажиры! Текущая высота самолёта {self.plane_model} составляет {self.current_height}м., а его скорость равна {self.current_speed}км/ч.)")
+        print("Пять минут - полёт нормальный!")
+
+    def airplane_landing(self):
+        """
+        Вывод полей конструктора (имитация посадки самолёта)
+        :return: None
+        """
+        self.current_height = f"{random.uniform(510, 400)} --> {random.uniform(380, 240)} --> {random.uniform(221, 157)}"
+        self.current_speed = f"{random.uniform(97, 73)} --> {random.uniform(72, 64)} --> {random.uniform(60, 51)}"
+        print(f"Уважаемые пассажиры! Просьба пристегнуть ремни безопасности, самолёт {self.plane_model} осуществляет посадку!\nТекущая высота: {self.current_height}м.\nТекущая скорость: {self.current_speed}км/ч.")
+
+    def end_of_flight(self):
+        """
+        Вывод полей конструктора (имитация окончания полёта самолёта)
+        :return: None
+        """
+        self.current_height = 0.0
+        self.current_speed = 0.0
+        print(f"Уважаемые пассажиры! Самолёт {self.plane_model} совершил успешную посадку! Благодарим, что выбрали нас!")
+
+
+airplanes = PassengerPlane("Airbus", "A380−800", 853, 0.0, 0)
+print(airplanes.plane_information())
+print(airplanes.airplane_takeoff())
+print(airplanes.airplane_flight())
+print(airplanes.airplane_landing())
+print(airplanes.end_of_flight())
