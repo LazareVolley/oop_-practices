20a3917bc70455329cdcc1c5b0fc7e1c022538ce
LazareVolley <LazareVolley@yandex.ru>
2024-05-23 23:42:25 +0300

    second example

--- hw_3/main_3.py
+++ hw_3/main_3.py
@@ -1,4 +1,4 @@
-#Задача №1.1
+#Задача №1.1 (Волшебник)
 class Wizard:
     def __init__(self, name: str, faculty: str, force_level: int, spells_list: list, status: bool):
         self.__name = name
@@ -55,5 +55,51 @@ class Wizard:
         else:
             self.__force_level -= force_level
 
-class Spell:
+    def __str__(self):
+        return wizard
+
+wizard = Wizard("Ron Weasley", "gryffindor", 10, "Eat Slugs", True)
+
+class Spels:
     pass
+
+#Задача №1.2 (Заклинание)
+class Spell:
+    def __init__(self, title: str, level: int, spell_type: str, description: str):
+        self.__title = title
+        self.__level = level
+        self.__spell_type = spell_type
+        self.__description = description
+
+    def get_title(self):
+        return self.__title
+
+    def get_level(self):
+        return self.__level
+
+    def get_spell_type(self):
+        return self.__spell_type
+
+    def get_description(self):
+        return self.__description
+
+    def set_title(self, title):
+        self.__title = title
+
+    def set_level(self, level):
+
+        if level < 0 or level > 10:
+            return f"Значение 'level' не может быть меньше 0 или больше 10!"
+        else:
+            self.__level = level
+
+    def set_spell_type(self, spell_type: str):
+        self.__spell_type = spell_type
+
+    def set_description(self, description):
+        self.__description = description
+
+    def __str__(self):
+        return spell
+
+spell = Spell("expecto patronum", 10, "explosive", "In order to use it, you must recall and feel your happiest memory (which is not easy with dementors), pull out your wand and say 'Expecto Patronum'")
